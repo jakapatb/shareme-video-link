44cfd814a209de8d778dc6e56777e46161f9f0f8
jakapatb <max.jakkapat@gmail.com>
2024-02-11 06:38:32 +0700

    fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -4,27 +4,31 @@ import { Metadata, ResolvingMetadata } from "next";
 import React from "react";
 
 type Props = {
-  params: { id: string }
-  searchParams: { [key: string]: string | string[] | undefined }
-}
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
 export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const {id} = params;
+  const { id } = params;
   const shortLink = await getShortLinkById(
     isNaN(parseInt(id)) ? 1 : parseInt(id)
   );
   return {
-    title:'test',
-    openGraph:{
-      videos:[shortLink.vdo]
-    }
-  }
+    title: "test",
+    openGraph: {
+      videos: [shortLink.vdo],
+    },
+  };
 }
 
-const ShareLinkPage = async ({ id }: { id: string }) => {
+const ShareLinkPage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
   const shortLink = await getShortLinkById(
     isNaN(parseInt(id)) ? 1 : parseInt(id)
   );
