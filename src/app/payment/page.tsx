a608b7e753a0ef6bc4638adbd21a3047aae6c69f
jakapatb <max.jakkapat@gmail.com>
2024-02-11 08:15:15 +0700

    try with payment page

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,4 +1,27 @@
+import type { Metadata } from "next/types";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Payment",
+  description: "Payment page",
+  openGraph: {
+    ttl: 600,
+    title: "Payment",
+    description: "Payment page",
+    type: "image" as any,
+    images: [
+      "https://preview.redd.it/inal9o589jhc1.jpeg?auto=webp&s=f549fd0d1d33655c82aba8b8e34da226ab085660",
+    ],
+  },
+  twitter: {
+    title: "Payment",
+    description: "Payment page",
+    card: "summary",
+    images: [
+      "https://preview.redd.it/inal9o589jhc1.jpeg?auto=webp&s=f549fd0d1d33655c82aba8b8e34da226ab085660",
+    ],
+  },
+};
 
 const PaymentPage = () => {
   return <div>PaymentPage</div>;
